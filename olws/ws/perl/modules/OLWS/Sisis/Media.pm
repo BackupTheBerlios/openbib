f5e459134b6e3554f44ba2972fa7459de5d9995a
flimm <flimm>
2006-05-04 20:26:28 +0000

    - Erweiterung um WebServices get_raw_XXX_by_katkey, um die Aufgeloesten Normdaten-BLOB's   zu einem Normdaten-Katalogschluessel zu bekommen. Damit sind u.a. inkrementelle Updates   einer Bestehenden Datenbank moeglich - Erweiterung um WebService get_title_katkeys_by_date, um entsprechend der Neuaufnahme-   bzw. Aenderungsdaten in titel_dupdaten fuer einen Zeitraum die entsprechenden Titel-   Katkeys zu bestimmen. Damit sind u.a. inkrementelle Updates einer Bestehenden Datenbank   moeglich. TODO: Bestimmung geloeschter Saetze

--- olws/ws/perl/modules/OLWS/Sisis/Media.pm
+++ olws/ws/perl/modules/OLWS/Sisis/Media.pm
@@ -63,4 +63,133 @@ sub get_native_title_by_katkey {
   return $titelref;
 }
 
+sub get_raw_tit_by_katkey {
+  
+  my ($class, $database, $katkey) = @_;
+
+  # Log4perl logger erzeugen
+
+  my $logger = get_logger();
+  
+  #####################################################################
+  # Verbindung zur SQL-Datenbank herstellen
+  
+  my $dbh=DBI->connect("DBI:$config{dbimodule}:dbname=$database;server=$config{dbserver};host=$config{dbhost};port=$config{dbport}", $config{dbuser}, $config{dbpasswd}) or $logger->error_die($DBI::errstr);
+  
+  my $sikfstabref=OLWS::Sisis::Data::get_sikfstabref($dbh,$database);
+  my $titref=OLWS::Sisis::Data::get_raw_titref_by_katkey($sikfstabref,$dbh,$database,$katkey);	
+  
+  return $titref;
+}
+
+
+sub get_raw_aut_by_katkey {
+  
+  my ($class, $database, $katkey) = @_;
+
+  # Log4perl logger erzeugen
+
+  my $logger = get_logger();
+  
+  #####################################################################
+  # Verbindung zur SQL-Datenbank herstellen
+  
+  my $dbh=DBI->connect("DBI:$config{dbimodule}:dbname=$database;server=$config{dbserver};host=$config{dbhost};port=$config{dbport}", $config{dbuser}, $config{dbpasswd}) or $logger->error_die($DBI::errstr);
+  
+  my $sikfstabref=OLWS::Sisis::Data::get_sikfstabref($dbh,$database);
+  my $autref=OLWS::Sisis::Data::get_raw_autref_by_katkey($sikfstabref,$dbh,$database,$katkey);	
+  
+  return $autref;
+}
+
+sub get_raw_kor_by_katkey {
+  
+  my ($class, $database, $katkey) = @_;
+
+  # Log4perl logger erzeugen
+
+  my $logger = get_logger();
+  
+  #####################################################################
+  # Verbindung zur SQL-Datenbank herstellen
+  
+  my $dbh=DBI->connect("DBI:$config{dbimodule}:dbname=$database;server=$config{dbserver};host=$config{dbhost};port=$config{dbport}", $config{dbuser}, $config{dbpasswd}) or $logger->error_die($DBI::errstr);
+  
+  my $sikfstabref=OLWS::Sisis::Data::get_sikfstabref($dbh,$database);
+  my $korref=OLWS::Sisis::Data::get_raw_korref_by_katkey($sikfstabref,$dbh,$database,$katkey);	
+  
+  return $korref;
+}
+
+sub get_raw_swt_by_katkey {
+  
+  my ($class, $database, $katkey) = @_;
+
+  # Log4perl logger erzeugen
+
+  my $logger = get_logger();
+  
+  #####################################################################
+  # Verbindung zur SQL-Datenbank herstellen
+  
+  my $dbh=DBI->connect("DBI:$config{dbimodule}:dbname=$database;server=$config{dbserver};host=$config{dbhost};port=$config{dbport}", $config{dbuser}, $config{dbpasswd}) or $logger->error_die($DBI::errstr);
+  
+  my $sikfstabref=OLWS::Sisis::Data::get_sikfstabref($dbh,$database);
+  my $swtref=OLWS::Sisis::Data::get_raw_swtref_by_katkey($sikfstabref,$dbh,$database,$katkey);	
+  
+  return $swtref;
+}
+
+sub get_raw_not_by_katkey {
+  
+  my ($class, $database, $katkey) = @_;
+
+  # Log4perl logger erzeugen
+
+  my $logger = get_logger();
+  
+  #####################################################################
+  # Verbindung zur SQL-Datenbank herstellen
+  
+  my $dbh=DBI->connect("DBI:$config{dbimodule}:dbname=$database;server=$config{dbserver};host=$config{dbhost};port=$config{dbport}", $config{dbuser}, $config{dbpasswd}) or $logger->error_die($DBI::errstr);
+  
+  my $sikfstabref=OLWS::Sisis::Data::get_sikfstabref($dbh,$database);
+  my $notref=OLWS::Sisis::Data::get_raw_notref_by_katkey($sikfstabref,$dbh,$database,$katkey);	
+  
+  return $notref;
+}
+
+sub get_title_katkeys_by_date {
+  my ($class, $database, $from_date, $to_date) = @_;
+
+  # Log4perl logger erzeugen
+
+  my $logger = get_logger();
+
+  $logger->debug("From: $from_date To: $to_date");  
+  #####################################################################
+  # Verbindung zur SQL-Datenbank herstellen
+  
+  my $dbh=DBI->connect("DBI:$config{dbimodule}:dbname=$database;server=$config{dbserver};host=$config{dbhost};port=$config{dbport}", $config{dbuser}, $config{dbpasswd}) or $logger->error_die($DBI::errstr);
+
+  my $result=$dbh->prepare("select katkey from $database.sisis.titel_exclude");
+  $result->execute();
+
+  my $excluded_katkey_ref=();
+  while (my $res_ref=$result->fetchrow_arrayref){
+    $excluded_katkey_ref->{$res_ref->[0]} = 1;
+  }	
+
+#  my $result=$dbh->prepare("select distinct dup.katkey as katkey from $database.sisis.titel_dupdaten as dup left join $database.sisis.titel_exclude on $database.sisis.titel_exclude.katkey != dup.katkey where (dup.datumaufn >= ? and dup.datumaufn <= ?) or (dup.datumaend >= ? and dup.datumaend <= ?)");
+  $result=$dbh->prepare("select dup.katkey from $database.sisis.titel_dupdaten as dup where (dup.datumaufn >= ? and dup.datumaufn <= ?) or (dup.datumaend >= ? and dup.datumaend <= ?)");
+  $result->execute($from_date,$to_date,$from_date,$to_date) or $logger->error_die($DBI::errstr);
+
+  my @katkeys=();
+  while (my $res_ref=$result->fetchrow_arrayref){
+    push @katkeys, $res_ref->[0] if (!defined $excluded_katkey_ref->{$res_ref->[0]});
+  }	
+
+  return \@katkeys;
+}
+
 1;
